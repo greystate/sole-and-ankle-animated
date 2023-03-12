473779f7b15c3bee79b4bff94db170d62991a84f
Chriztian Steinmeier <chriztian@steinmeier.dk>
2023-03-12 15:10:41 +0100

    Add Shoe hover

--- src/components/ShoeCard/ShoeCard.js
+++ src/components/ShoeCard/ShoeCard.js
@@ -34,12 +34,12 @@ const ShoeCard = ({
   return (
     <Link href={`/shoe/${slug}`}>
       <Wrapper>
+        {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
+        {variant === 'new-release' && (
+            <NewFlag>Just released!</NewFlag>
+        )}
         <ImageWrapper>
           <Image alt="" src={imageSrc} />
-          {variant === 'on-sale' && <SaleFlag>Sale</SaleFlag>}
-          {variant === 'new-release' && (
-            <NewFlag>Just released!</NewFlag>
-          )}
         </ImageWrapper>
         <Spacer size={12} />
         <Row>
@@ -73,15 +73,26 @@ const Link = styled.a`
   color: inherit;
 `;
 
-const Wrapper = styled.article``;
+const Wrapper = styled.article`
+  position:relative;
+`;
 
 const ImageWrapper = styled.div`
   position: relative;
+  border-radius: 16px 16px 4px 4px;
+  overflow: hidden;
+  z-index: 1;
 `;
 
 const Image = styled.img`
+  transition-duration: 380ms;
   width: 100%;
-  border-radius: 16px 16px 4px 4px;
+  display: block;
+  &:hover {
+    transition: transform 100ms ease-in-out;
+    transform: scale(1.1);
+    transform-origin: 50% 80%;
+  }
 `;
 
 const Row = styled.div`
@@ -121,6 +132,8 @@ const Flag = styled.div`
   font-weight: ${WEIGHTS.bold};
   color: var(--color-white);
   border-radius: 2px;
+  z-index: 5;
+
 `;
 
 const SaleFlag = styled(Flag)`
